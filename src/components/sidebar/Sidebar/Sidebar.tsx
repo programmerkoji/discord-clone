ad92e7930b43fd30998dc59d036dc301c265a99b
KojiSato <utautaouka@gmail.com>
2023-03-25 14:23:26 +0900

    サイドバーのHTML,CSSを作成

--- src/components/sidebar/Sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar/Sidebar.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
 import "./Sidebar.scss";
+import SidebarChannel from "./SidebarChannel";
 
 const Sidebar = () => {
 	return (
@@ -15,6 +21,54 @@ const Sidebar = () => {
       <div className="sidebarRight">
         <div className="sidebarTop">
           <h3>Discord</h3>
+          <ExpandMoreIcon />
+        </div>
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarIcon" />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+          </div>
+        </div>
+        
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarIcon" />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="./icon.png" alt="" />
+              <div className="accountName">
+                <h4>Shincode</h4>
+                <span>#8162</span>
+              </div>
+            </div>
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
+          </div>
+
         </div>
       </div>
 		</div>
---
+++ src/components/sidebar/Sidebar/SidebarChannel.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SidebarChannel.scss";
+
+const SidebarChannel = () => {
+  return <div className="sidebarChannel">
+    <h4>
+      <span className="sidebarChannelHash">#</span>
+      udemy
+    </h4>
+  </div>;
+};
+
+export default SidebarChannel;
