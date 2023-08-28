32a1efc38fc3e258bf4b03a19f27fcb16a49a108
Koji Sato <utautaouka@gmail.com>
2023-08-28 21:28:26 +0900

    サイドバーのチャンネル追加を実装

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,17 +4,27 @@ import AddIcon from "@mui/icons-material/Add";
 import MicIcon from "@mui/icons-material/Mic";
 import HeadphonesIcon from "@mui/icons-material/Headphones";
 import SettingsIcon from "@mui/icons-material/Settings";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
 import "./Sidebar.scss";
 import SidebarChannel from "./SidebarChannel";
 import { useAppSelector } from "../../app/hooks";
 import useCollection from "../../hooks/useCollection";
+import { addDoc, collection } from "firebase/firestore";
 
 const Sidebar = () => {
 	const user = useAppSelector((state) => state.user);
-	console.log(useCollection("channels"));
-	
+
 	const { documents: channels } = useCollection("channels");
+
+	const addChannel = async () => {
+		let channelName: string | null = prompt("新しいチャンネルを作成します");
+		
+		if (channelName) {
+			await addDoc(collection(db, "channels"), {
+				channelName: channelName,
+			}); 
+		}
+	}
 
 	return (
 		<div className="sidebar">
@@ -37,7 +47,7 @@ const Sidebar = () => {
 							<ExpandMoreIcon />
 							<h4>プログラミングチャンネル</h4>
 						</div>
-						<AddIcon className="sidebarIcon" />
+						<AddIcon className="sidebarIcon" onClick={() => addChannel()} />
 					</div>
 					{channels.map((channel) => (
 						<div className="sidebarChannelList">
